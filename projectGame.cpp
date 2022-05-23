c529c695142328347e61246fad90fde50db275a3
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-23 18:54:44 -0300

    created character logic

--- projectGame.cpp
+++ projectGame.cpp
@@ -5,37 +5,55 @@
 
 using namespace std;
 
-// Functions declarations
+// Functions declarations ------------------------------------------------
 
 void gotoxy(int x, int y);
+void set_cursor(bool visible);
 void drawContainer(int height, int width);
 void asciiValues();
+void direction(char dir);
+void moving(char dir);
+
+// Global variables ------------------------------------------------------
+
+const int WIDTH = 118;
+const int HEIGHT = 28;
+const int CHARACTER = 2;
+int x_init = 5;
+int y_init = 5;
+char keyInput;
+
+// Main function ---------------------------------------------------------
 
 int main()
 {	
-	char keyInput;
-	const int WIDTH = 118;
-	const int HEIGHT = 28;
-	int x_init = 5;
-	int y_init = 5;
+	set_cursor(false);
    	drawContainer(HEIGHT,WIDTH);
    	gotoxy(x_init,y_init);
-   	printf("%c",254);
-   	cin >> keyInput;
-   	getchar();
-   	switch(keyInput)
+   	printf("%c",CHARACTER);
+   	
+   	while(keyInput != 'q')
    	{
-   		case 'w': 
-		   	gotoxy(x_init,y_init-1);
-   		   	printf("%c",254);
-   		break;
-   		case 'a':
-   		break;
-   		case 's':
-   		break;
-   		case 'd':
-   		break;
+   		keyInput = getch();
+   		if(y_init == HEIGHT || y_init == 1) keyInput = 'q';
+   		if(x_init == WIDTH) x_init = 2;
+		if(x_init == 1) x_init = WIDTH;    
+		Sleep(100);
+   		switch(keyInput)
+   		{
+   			case 'w': 
+			   	direction('u');
+   				moving('u');
+   			break;
+   			case 'a': direction('l');
+   				break;
+   			case 's': direction('d');
+   				break;
+   			case 'd': direction('r');
+   				break;
+		}	
 	}
+	
    	getch();
    	return 0;
 }
@@ -46,6 +64,13 @@ void gotoxy(int x, int y)
     coordinate.X = x;
     coordinate.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
+
+void set_cursor(bool visible){
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = visible;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 }
 
 void drawContainer(int height, int width)
@@ -72,6 +97,32 @@ void asciiValues()
    	{
    		printf("%d: %c\n",i,i);
    	}
+}
+
+void direction(char dir)
+{
+	gotoxy(x_init,y_init);
+  	printf("%c",255);
+   	if(dir == 'u') gotoxy(x_init,y_init-=1);
+   	if(dir == 'd') gotoxy(x_init,y_init+=1);
+   	if(dir == 'l') gotoxy(x_init-=1,y_init);
+   	if(dir == 'r') gotoxy(x_init+=1,y_init);
+   	printf("%c",CHARACTER);
+}
+
+void moving(char dir)
+{
+	if(dir == 'u') 
+	{
+		while(keyInput == 'w')
+		{
+			direction('u');
+			Sleep(100);
+		}	
+	}
+   	if(dir == 'd') gotoxy(x_init,y_init+=1);
+   	if(dir == 'l') gotoxy(x_init-=1,y_init);
+   	if(dir == 'r') gotoxy(x_init+=1,y_init);
 }
 
 
