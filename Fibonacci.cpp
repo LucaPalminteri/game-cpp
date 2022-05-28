231525aaf56b86a3e24e9a8159484dd431b28dad
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-28 10:30:23 -0300

    added Fibonacci serie to calculate perfect rectangles

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,15 +2,17 @@
 #include<stdio.h>
 #include<math.h>
 
+void calcPhi(int iter);
+
+double const PHI = (1+sqrt(5))/2;
+
 int main(){
 	
 	int const ITERATE = 30;
 	long int n[ITERATE];
-	double phi;
+	double phiCount;
 	n[0] = 1;
 	n[1] = 1;
-	
-	double goldn = (1+sqrt(5))/2;
 	
 	printf("\n\t~~~~~~~~~~~~ FIBONACCI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	printf("\t|\t\t\t\t\t\t\t|\n");
@@ -20,17 +22,32 @@ int main(){
 		if(i >= 2) 
 		{
 			n[i] = n[i-2] + n[i-1];
-			phi = (double) n[i]/n[i-1];
+			phiCount = (double) n[i]/n[i-1];
 		}
 		
-		printf("\t|\t%d\t%d\t%.20f\t\t|\n",i+1,n[i],phi);	
+		printf("\t|\t%d\t%d\t%.20f\t\t|\n",i+1,n[i],phiCount);	
 	}
 	
 	printf("\t|\t\t\t\t\t\t\t|\n");
 	printf("\t---------------------------------------------------------\n");
 	
-	printf("\n\t\t\t%.20f",goldn);
+	printf("\n\t\t\t%.20f\n\n",PHI);
+	
+	calcPhi(10);
+	
 	
 	getch();
 	return 0;
+}
+
+void calcPhi(int iter)
+{
+	double h,a=1;
+	
+	for(int i=0;i<iter;i++)
+	{
+		h = a * PHI;
+		printf("\n\t%.8f \tx \t%.8f\n",a,h);
+		a = h;
+	}
 }
