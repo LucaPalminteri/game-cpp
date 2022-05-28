50417f803bba14d063387e5827066d3d77c6225c
LucaPalminteri <lucapalminteri02@gmail.com>
2022-05-27 22:00:09 -0300

    added phi reference and fix overmemory bug

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,20 +1,35 @@
 #include<conio.h>
 #include<stdio.h>
+#include<math.h>
 
 int main(){
 	
-	int const ITERATE = 20;
-	int n[ITERATE];
+	int const ITERATE = 30;
+	long int n[ITERATE];
+	double phi;
 	n[0] = 1;
 	n[1] = 1;
 	
-	printf("\n");
+	double goldn = (1+sqrt(5))/2;
+	
+	printf("\n\t~~~~~~~~~~~~ FIBONACCI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("\t|\t\t\t\t\t\t\t|\n");
 	
 	for(int i=0;i<ITERATE;i++)
 	{
-		if(i >= 2) n[i] = n[i-2] + n[i-1];
-		printf("\t%d: \t%d\n",i,n[i]);	
+		if(i >= 2) 
+		{
+			n[i] = n[i-2] + n[i-1];
+			phi = (double) n[i]/n[i-1];
+		}
+		
+		printf("\t|\t%d\t%d\t%.20f\t\t|\n",i+1,n[i],phi);	
 	}
+	
+	printf("\t|\t\t\t\t\t\t\t|\n");
+	printf("\t---------------------------------------------------------\n");
+	
+	printf("\n\t\t\t%.20f",goldn);
 	
 	getch();
 	return 0;
